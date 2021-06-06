cc872b84b2944d8b48ff666a1d8a034fa22744ef
zakariya <97zakariyameraj@gmail.com>
2021-06-06 19:57:01 +0000

    sec commit

--- src/main/java/com/zak/hibernet/demo/App.java
+++ src/main/java/com/zak/hibernet/demo/App.java
@@ -7,6 +7,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("hi");
         HibernateUtil h= new HibernateUtil();
         SessionFactory sf=h.getSessionFactory();
         
