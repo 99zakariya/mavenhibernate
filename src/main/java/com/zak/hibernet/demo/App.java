c03f41bb07f497b4db9cae61e2408c7ad6e7cccb
zakariya <97zakariyameraj@gmail.com>
2021-06-07 07:18:52 +0000

    update

--- src/main/java/com/zak/hibernet/demo/App.java
+++ src/main/java/com/zak/hibernet/demo/App.java
@@ -5,22 +5,41 @@ import org.hibernate.SessionFactory;
 
 public class App 
 {
+    SessionFactory sf;
+    public App(){
+        HibernateUtil h= new HibernateUtil();
+         sf=h.getSessionFactory();
+    }
     public static void main( String[] args )
     {
         System.out.println("hi");
-        HibernateUtil h= new HibernateUtil();
-        SessionFactory sf=h.getSessionFactory();
         
-     //   Item i=new Item();
-      //  i.setName("vscode");
-       // i.setId(2);
         
+        Item i=new Item();
+        i.setName("vscode");
+        i.setId(2);
+
+        App ob=new App();
+        ob.add(i);
+        ob.print();
+      
+      
+        
+        System.out.println("bye");
+    }
+    public void add(Item i)
+    {
         Session s=sf.openSession();
         s.beginTransaction();
-       // s.save(i);
-       Item i = (Item)s.get(Item.class,2);
-       System.out.println(i);
+        s.save(i);
         s.getTransaction().commit();
-        System.out.println("bye");
+    }
+    public void print()
+    {
+        Session s=sf.openSession();
+        s.beginTransaction();
+        Item i = (Item)s.get(Item.class,2);
+        System.out.println(i);
+        s.getTransaction().commit();
     }
 }
